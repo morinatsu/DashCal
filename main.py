9c09eb71f1a89111033a1b089dfdecec27853b4c
Natsuki Mori <morinatsu@gmail.com>
2024-08-18 09:24:08 +0900

    judge if NoneType time_created

--- main.py
+++ main.py
@@ -26,55 +26,66 @@ bucket_name = os.environ["BUCKET_NAME"]
 japan_time = pytz.timezone('Asia/Tokyo')
 
 
+def get_user():
+    """get username from GET parameter"""
+    username = request.args.get("user", "")
+    if username:
+        user = re.match(r'(^[A-Za-z][A-Za-z0-9-]+$)', username)
+    else:
+        user = None
+    if user:
+        return user.group(1)
+    raise ValueError
+
+
+def get_from_cache(user, limit):
+    """get html from cache"""
+    logging.info('get_from_cahce')
+    bucket = client.bucket(bucket_name)
+    blob = bucket.blob(user)
+    if blob.exists() is True:
+        blob.reload()
+        logging.debug('cache.created: %s, limit: %s',
+                      blob.time_created,
+                      limit)
+        if (blob.time_created is None) or \
+           (blob.time_created > limit):
+            return blob.download_as_text()
+    return None
+
+
+def put_to_cache(user, html):
+    """put html to cache"""
+    logging.info('put_to_cahce')
+    bucket = client.bucket(bucket_name)
+    blob = bucket.blob(user)
+    blob.upload_from_string(html)
+
+
+def fetch_from_site(user):
+    """fetch html from comic-dash website"""
+    logging.info('fetch_from_site')
+    url = "https://ckworks.jp/comicdash/calendar/" + user
+    try:
+        res = requests.get(url, timeout=60.0)
+    except requests.Timeout:
+        logging.error('request timeout, user: %s', user)
+        return None
+    except Exception as err:
+        logging.error('Unexpected %s, %s', err, type(err))
+        logging.error(traceback.format_exc())
+        raise
+    # Convert to ical data
+    logging.info('res.content: %s', res.content[0:20])
+    if (res.content is None or res.content == b'\n'):
+        return None
+    put_to_cache(user, res.content)
+    return res.content
+
+
 @app.route('/ical')
 def convert():
     """Convert to ical data"""
-    def get_user():
-        """get username from GET parameter"""
-        username = request.args.get("user", "")
-        if username:
-            user = re.match(r'(^[A-Za-z][A-Za-z0-9-]+$)', username)
-        else:
-            user = None
-        if user:
-            return user.group(1)
-        raise ValueError
-
-    def get_from_cache(user, limit):
-        """get html from cache"""
-        logging.info('get_from_cahce')
-        bucket = client.bucket(bucket_name)
-        blob = bucket.blob(user)
-        if blob.exists() is True:
-            if blob.time_created > limit:
-                return blob.download_as_text()
-        return None
-
-    def put_to_cache(user, html):
-        """put html to cache"""
-        logging.info('put_to_cahce')
-        bucket = client.bucket(bucket_name)
-        blob = bucket.blob(user)
-        blob.upload_from_string(html)
-
-    def fetch_from_site(user):
-        logging.info('fetch_from_site')
-        url = "https://ckworks.jp/comicdash/calendar/" + user
-        try:
-            res = requests.get(url, timeout=60.0)
-        except requests.Timeout:
-            logging.error('request timeout, user: %s', user)
-            return None
-        except Exception as err:
-            logging.error('Unexpected %s, %s', err, type(err))
-            logging.error(traceback.format_exc())
-            raise
-        # Convert to ical data
-        logging.info('res.content: %s', res.content[0:20])
-        if (res.content is None or res.content == b'\n'):
-            return None
-        put_to_cache(user, res.content)
-        return res.content
 
     # get user
     try:
